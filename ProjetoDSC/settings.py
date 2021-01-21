ba42789a708b2037306130b1d58f8429c33b7bc7
kanatius <natanalmeidadelima@gmail.com>
2021-01-21 14:03:32 -0300

    database configured

--- ProjetoDSC/settings.py
+++ ProjetoDSC/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'ProjetoDSC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'impressoes_dsc',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
