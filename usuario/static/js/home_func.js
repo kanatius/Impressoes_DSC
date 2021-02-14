c0a810572d564ccf0a5520d887f6eb60aaced077
kanatius <natanalmeidadelima@gmail.com>
2021-02-14 13:06:47 -0300

    details modal working on staff area

--- usuario/static/js/home_func.js
+++ usuario/static/js/home_func.js
@@ -12,3 +12,26 @@ function openModalCorfimPrinted(object){
     $("#id_impressao_modal").val(idImpressao);
 
 }
+
+function fillDetailsModalData(object){
+
+    parent = $(object).parent();
+    tr = $(parent).parent();
+
+    td_data = $(tr).children(".dados_impressao")[0];
+
+    comentario = $(td_data).children(".impressao_comentario")[0].value;
+    turma = $(td_data).children(".impressao_turma")[0].value;
+    filename = $(td_data).children(".nome_impressao")[0].value;
+    cliente = $(td_data).children(".cliente_nome")[0].value;
+    data_pedido =  $(td_data).children(".data_pedido")[0].value;
+
+    console.log(comentario, turma, filename, cliente, data_pedido);
+
+
+    $("#filename_details_modal").html(filename);
+    $("#data_pedido_details_modal").html(data_pedido);
+    $("#cliente_details_modal").html(cliente);
+    $("#turma_details_modal").html(turma);
+    $("#comentario_details_modal").html(comentario);
+}
